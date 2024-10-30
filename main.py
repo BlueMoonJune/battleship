91f4d5a189881c96eb9da4542339429dbc31deb1
June <bluemoonjune0403@gmail.com>
2024-10-30 08:50:43 -0700

    mark and count misses

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ w = 5
 h = 5
 
 # the "ocean"
-board = [["O"] * w] * h
+board = [["O"] * w for _ in range(0, h)]
 
 # prints a 2D array nicely
 def print2D(arr):
@@ -15,14 +15,24 @@ def print2D(arr):
 
 ship_pos = (random.randint(0, w), random.randint(0, h))
 
+misses = 0
+
 # this will keep re-printing the "Ocean" every turn
 # untill the user gives a certain input
 while True:
     print2D(board)
     user = input("Where would you like to hit horozontly? [x, y]")
-    if user == f"{ship_pos[0]}, {ship_pos[1]}":
+    x = int(user[:user.find(",")])
+    y = int(user[user.find(" ") + 1:])
+    if 0 > x >= w or 0 > y >= h:
+        print("outside of board")
+    elif (x, y) == ship_pos:
         print("Hit! you win!")
+        break
     elif "stop" == user:
         break
     else:
         print("Miss! try again.")
+        board[y][x] = "X"
+        misses += 1
+
